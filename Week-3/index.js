787022e01c97ae46e127d3a38e69d53bce3bf24b
DAVIDCON15 <78663303+DAVIDCON15@users.noreply.github.com>
2021-02-13 12:40:21 -0800

    made multiples of five in for loop

---
+++ Week-3/index.js
@@ -0,0 +1,11 @@
+// 1) Install NPM package, check
+// 2) Use for loops, print out multiplication tables. Challenge: Use list for any number on multiplication chart.
+// 3) use algorithm to collect data is Username, password. display input box. Use Alert box.
+
+var math = [5,10,15,20,25,30,35,40,45,50,55,60];
+
+for(var i=0; i<12; i++){
+    console.log(math[i]);
+}
+
+
